d2da30c74b53ed3b0d18b159a84e966260c3d749
Jack Purvis <jack.v.purvis@gmail.com>
2024-09-07 20:10:31 +1200

    Engine update loop + stats

--- engine/src/engine.rs
+++ engine/src/engine.rs
@@ -1,7 +1,34 @@
-pub struct Engine {}
+use crate::stats::Stats;
+
+pub struct Engine {
+    stats: Stats,
+}
 
 impl Engine {
     pub fn run() {
+        let mut engine = Self::new();
+
         println!("Engine running!");
+
+        loop {
+            engine.update();
+
+            // TODO: adjust sleep duration dynamically or find a way to integrate into an event loop e.g: tao, winit
+            std::thread::sleep(std::time::Duration::from_millis(2));
+        }
+    }
+
+    pub fn new() -> Self {
+        Self {
+            stats: Stats::new(),
+        }
+    }
+
+    pub fn update(&mut self) {
+        self.stats.before_update();
+
+        // TODO: implement update body
+
+        self.stats.after_update();
     }
 }
