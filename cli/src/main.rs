890344145959fecf46ecd7e33dbc4980414dbe89
Jack Purvis <jack.v.purvis@gmail.com>
2024-09-11 22:18:31 +1200

    EngineBuilder + event loop for cli program

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,53 @@
-use engine::engine::Engine;
+use std::sync::Arc;
+
+use engine::engine::EngineBuilder;
 
 fn main() {
-    Engine::run();
+    let mut engine = EngineBuilder::default().build();
+
+    let event_loop = tao::event_loop::EventLoop::new();
+
+    let create_window_callback = Box::new(|window_builder: tao::window::WindowBuilder| {
+        let window = window_builder
+            .build(&event_loop)
+            .expect("Unexpected: could not build window");
+
+        Arc::new(window)
+    });
+
+    let render_texture = engine.create_render_texture(600, 400);
+    engine.create_display(
+        "Display".into(),
+        600,
+        400,
+        &render_texture,
+        create_window_callback,
+    );
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = tao::event_loop::ControlFlow::Wait;
+
+        match event {
+            tao::event::Event::WindowEvent {
+                event: tao::event::WindowEvent::CloseRequested,
+                window_id: _,
+                ..
+            } => {
+                std::process::exit(0);
+            }
+
+            tao::event::Event::WindowEvent {
+                event: tao::event::WindowEvent::Destroyed,
+                window_id: _,
+                ..
+            } => {
+                *control_flow = tao::event_loop::ControlFlow::Exit;
+            }
+
+            tao::event::Event::MainEventsCleared => {
+                engine.update();
+            }
+            _ => (),
+        }
+    });
 }
--- engine/src/engine.rs
+++ engine/src/engine.rs
@@ -1,34 +1,95 @@
+use std::sync::Arc;
+
+use display::display::Display;
+use tao::window::{Window, WindowBuilder};
+use tokio::runtime::Runtime;
+use wgpu::{instance::Instance, render_texture::RenderTexture};
+
 use crate::stats::Stats;
 
+#[derive(Default)]
+pub struct EngineBuilder {
+    runtime: Option<Runtime>,
+}
+
+impl EngineBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn with_runtime(mut self, runtime: Runtime) -> Self {
+        self.runtime = Some(runtime);
+        self
+    }
+
+    pub fn build(self) -> Engine {
+        let runtime = self.runtime.unwrap_or(
+            tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()
+                .expect("Unexpected: could not create tokio runtime"),
+        );
+
+        Engine::new(runtime)
+    }
+}
+
 pub struct Engine {
+    runtime: Runtime,
+    displays: Vec<Display>,
     stats: Stats,
+    wgpu_instance: Instance,
 }
 
 impl Engine {
-    pub fn run() {
-        let mut engine = Self::new();
-
-        println!("Engine running!");
-
-        loop {
-            engine.update();
-
-            // TODO: adjust sleep duration dynamically or find a way to integrate into an event loop e.g: tao, winit
-            std::thread::sleep(std::time::Duration::from_millis(2));
-        }
-    }
-
-    pub fn new() -> Self {
+    fn new(runtime: Runtime) -> Self {
         Self {
+            runtime,
+            displays: Vec::new(),
             stats: Stats::new(),
+            wgpu_instance: Instance::default(),
         }
     }
 
     pub fn update(&mut self) {
         self.stats.before_update();
 
-        // TODO: implement update body
+        for display in &mut self.displays {
+            display.render();
+        }
 
         self.stats.after_update();
+    }
+
+    pub fn create_render_texture(&self, width: u32, height: u32) -> RenderTexture {
+        // TODO: store render textures in engine struct
+        self.runtime
+            .block_on(async { RenderTexture::new(&self.wgpu_instance, width, height).await })
+    }
+
+    pub fn create_display<F>(
+        &mut self,
+        title: String,
+        width: u32,
+        height: u32,
+        render_texture: &RenderTexture,
+        create_window_callback: F,
+    ) where
+        F: Fn(WindowBuilder) -> Arc<Window>,
+    {
+        let window_builder = WindowBuilder::new()
+            .with_title(title)
+            .with_inner_size(tao::dpi::PhysicalSize::new(width, height));
+
+        let window = create_window_callback(window_builder);
+
+        let display = self
+            .runtime
+            .block_on(async {
+                Display::new(&self.wgpu_instance, window, render_texture.texture_view()).await
+            })
+            .into();
+
+        self.displays.push(display);
     }
 }
