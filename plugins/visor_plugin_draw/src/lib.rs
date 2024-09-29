eab409defd801141dd434a3f5d4b742669f83ada
Jack Purvis <jack.v.purvis@gmail.com>
2024-09-29 15:51:13 +1300

    Circle, ellipse, square, rect for draw plugin

--- plugins/visor_plugin_draw/src/lib.rs
+++ plugins/visor_plugin_draw/src/lib.rs
@@ -5,7 +5,7 @@ pub struct DrawPlugin;
 
 extension!(
     extension,
-    ops = [op_draw_rect],
+    ops = [op_draw_ellipse, op_draw_rect],
     esm_entry_point = "visor:draw",
     esm = [
         dir "src",
@@ -20,12 +20,23 @@ impl Plugin for DrawPlugin {
 }
 
 #[op2(fast)]
-fn op_draw_rect(state: &mut OpState, x: f32, y: f32, d: f32) {
+fn op_draw_ellipse(state: &mut OpState, x: f32, y: f32, width: f32, height: f32) {
+    let draw = state.borrow::<Draw>();
+
+    draw.inner
+        .ellipse()
+        .x_y(x, y)
+        .w_h(width, height)
+        .color(nannou::prelude::RED);
+}
+
+#[op2(fast)]
+fn op_draw_rect(state: &mut OpState, x: f32, y: f32, width: f32, height: f32) {
     let draw = state.borrow::<Draw>();
 
     draw.inner
         .rect()
         .x_y(x, y)
-        .w_h(d, d)
+        .w_h(width, height)
         .color(nannou::prelude::RED);
 }
--- plugins/visor_plugin_draw/src/visor-draw.js
+++ plugins/visor_plugin_draw/src/visor-draw.js
@@ -1,5 +1,23 @@
-export function rect(x, y, d) {
+export function circle(x, y, size) {
+  const { op_draw_ellipse } = Deno.core.ops;
+
+  op_draw_ellipse(x, y, size, size);
+}
+
+export function ellipse(x, y, width, height) {
+  const { op_draw_ellipse } = Deno.core.ops;
+
+  op_draw_ellipse(x, y, width, height);
+}
+
+export function square(x, y, size) {
   const { op_draw_rect } = Deno.core.ops;
 
-  op_draw_rect(x, y, d);
+  op_draw_rect(x, y, size, size);
+}
+
+export function rect(x, y, width, height) {
+  const { op_draw_rect } = Deno.core.ops;
+
+  op_draw_rect(x, y, width, height);
 }
