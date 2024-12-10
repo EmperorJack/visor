4623988f1a27a708e86b8851fc90de8d326bee83
Jack Purvis <jack.v.purvis@gmail.com>
2024-12-11 11:43:34 +1300

    Build out draw plugin to handle basic transforms

--- plugins/visor_plugin_draw/src/lib.rs
+++ plugins/visor_plugin_draw/src/lib.rs
@@ -1,11 +1,23 @@
+use std::collections::HashMap;
+
 use deno_core::{extension, op2, Extension, OpState};
-use visor_engine::{draw::Draw, plugin::Plugin};
+use visor_engine::{draw::Draw, plugin::Plugin, sketch::SketchId, store::Store, Runtime};
 
 pub struct DrawPlugin;
 
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+struct DrawId(u32);
+type DrawMap = HashMap<DrawId, Draw>;
+
+impl DrawId {
+    fn increment(&mut self) {
+        self.0 += 1
+    }
+}
+
 extension!(
     extension,
-    ops = [op_draw_ellipse, op_draw_rect],
+    ops = [op_draw_ellipse, op_draw_rect, op_draw_translate, op_draw_rotate],
     esm_entry_point = "visor:draw",
     esm = [
         dir "src",
@@ -17,11 +29,41 @@ impl Plugin for DrawPlugin {
     fn extension(&self) -> Extension {
         extension::init_ops_and_esm()
     }
+
+    fn before_sketch_update(&self, _sketch_id: &SketchId, runtime: &mut Runtime, _store: &Store) {
+        runtime.put_state(DrawMap::default());
+        runtime.put_state(DrawId(0));
+    }
+}
+
+fn get_draw(state: &OpState, id: DrawId) -> &Draw {
+    if id.0 == 0 {
+        return state.borrow::<Draw>();
+    }
+
+    if let Some(draw) = state.borrow::<DrawMap>().get(&id) {
+        return draw;
+    }
+
+    // Return base draw if the given draw ID is invalid
+    return state.borrow::<Draw>();
+}
+
+fn store_draw(state: &mut OpState, draw: Draw) -> DrawId {
+    let mut id = state.take::<DrawId>();
+    id.increment();
+
+    let draw_map = state.borrow_mut::<DrawMap>();
+    draw_map.insert(id, draw);
+
+    state.put(id);
+
+    id
 }
 
 #[op2(fast)]
-fn op_draw_ellipse(state: &mut OpState, x: f32, y: f32, width: f32, height: f32) {
-    let draw = state.borrow::<Draw>();
+fn op_draw_ellipse(state: &OpState, id: u32, x: f32, y: f32, width: f32, height: f32) {
+    let draw = get_draw(state, DrawId(id));
 
     draw.inner
         .ellipse()
@@ -31,12 +73,30 @@ fn op_draw_ellipse(state: &mut OpState, x: f32, y: f32, width: f32, height: f32)
 }
 
 #[op2(fast)]
-fn op_draw_rect(state: &mut OpState, x: f32, y: f32, width: f32, height: f32) {
-    let draw = state.borrow::<Draw>();
+fn op_draw_rect(state: &OpState, id: u32, x: f32, y: f32, width: f32, height: f32) {
+    let draw = get_draw(state, DrawId(id));
 
     draw.inner
         .rect()
         .x_y(x, y)
         .w_h(width, height)
         .color(nannou::prelude::RED);
+}
+
+#[op2(fast)]
+fn op_draw_translate(state: &mut OpState, id: u32, x: f32, y: f32) -> u32 {
+    let draw = get_draw(state, DrawId(id));
+
+    let draw = draw.inner.x_y(x, y);
+
+    store_draw(state, Draw { inner: draw }).0
+}
+
+#[op2(fast)]
+fn op_draw_rotate(state: &mut OpState, id: u32, radians: f32) -> u32 {
+    let draw = get_draw(state, DrawId(id));
+
+    let draw = draw.inner.rotate(radians);
+
+    store_draw(state, Draw { inner: draw }).0
 }
--- plugins/visor_plugin_draw/src/visor-draw.js
+++ plugins/visor_plugin_draw/src/visor-draw.js
@@ -1,28 +1,44 @@
-export function circle(x, y, size) {
-  const { op_draw_ellipse } = Deno.core.ops;
+const { op_draw_ellipse, op_draw_rect, op_draw_translate, op_draw_rotate } =
+  Deno.core.ops;
 
-  op_draw_ellipse(x, y, size, size);
+class Draw {
+  #id;
+
+  constructor(id) {
+    this.#id = id;
+  }
+
+  copy() {
+    return new Draw(this.#id);
+  }
+
+  // Shapes
+  circle(x, y, size) {
+    op_draw_ellipse(this.#id, x, y, size, size);
+  }
+  ellipse(x, y, width, height) {
+    op_draw_ellipse(this.#id, x, y, width, height);
+  }
+  square(x, y, size) {
+    op_draw_rect(this.#id, x, y, size, size);
+  }
+  rect(x, y, width, height) {
+    op_draw_rect(this.#id, x, y, width, height);
+  }
+
+  // Transforms
+  translate(x, y) {
+    const nextId = op_draw_translate(this.#id, x, y);
+    return new Draw(nextId);
+  }
+  rotate(radians) {
+    const nextId = op_draw_rotate(this.#id, radians);
+    return new Draw(nextId);
+  }
 }
 
-export function ellipse(x, y, width, height) {
-  const { op_draw_ellipse } = Deno.core.ops;
-
-  op_draw_ellipse(x, y, width, height);
+function createDraw() {
+  return new Draw(0);
 }
 
-export function square(x, y, size) {
-  const { op_draw_rect } = Deno.core.ops;
-
-  op_draw_rect(x, y, size, size);
-}
-
-export function rect(x, y, width, height) {
-  const { op_draw_rect } = Deno.core.ops;
-
-  op_draw_rect(x, y, width, height);
-}
-
-globalThis.circle = circle;
-globalThis.ellipse = ellipse;
-globalThis.square = square;
-globalThis.rect = rect;
+globalThis.createDraw = createDraw;
