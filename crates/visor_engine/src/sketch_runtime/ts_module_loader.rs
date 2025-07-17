e38354a1042782171fabfa149b24317814b82509
Jack Purvis <jack.v.purvis@gmail.com>
2025-07-17 22:16:21 +1200

    Ts module loader sync response

--- crates/visor_engine/src/sketch_runtime/ts_module_loader.rs
+++ crates/visor_engine/src/sketch_runtime/ts_module_loader.rs
@@ -4,7 +4,7 @@ use deno_ast::{EmitOptions, MediaType, ParseParams};
 use deno_core::{
     ModuleCodeString, ModuleLoadResponse, ModuleLoader, ModuleName, ModuleSource, ModuleSourceCode,
     ModuleSpecifier, ModuleType, RequestedModuleType, ResolutionKind, SourceMapData,
-    error::ModuleLoaderError, futures::FutureExt, url::Url,
+    error::ModuleLoaderError, url::Url,
 };
 use deno_error::JsErrorBox;
 
@@ -29,93 +29,91 @@ impl ModuleLoader for TsModuleLoader {
     ) -> ModuleLoadResponse {
         let module_specifier = module_specifier.clone();
 
-        // TODO: why async here?
-        ModuleLoadResponse::Async(
-            async move {
-                let path = module_specifier.to_file_path().map_err(|_| {
-                    JsErrorBox::generic(format!(
-                        "Could not resolve file path for import \"{}\"",
-                        module_specifier
-                    ))
-                })?;
+        let module_load = move || {
+            let path = module_specifier.to_file_path().map_err(|_| {
+                JsErrorBox::generic(format!(
+                    "Could not resolve file path for import \"{}\"",
+                    module_specifier
+                ))
+            })?;
 
-                let media_type = MediaType::from_path(&path);
+            let media_type = MediaType::from_path(&path);
 
-                let (module_type, should_transpile) = match MediaType::from_path(&path) {
-                    MediaType::JavaScript | MediaType::Mjs | MediaType::Cjs => {
-                        (ModuleType::JavaScript, false)
-                    }
-                    MediaType::Jsx => (ModuleType::JavaScript, true),
-                    MediaType::TypeScript
-                    | MediaType::Mts
-                    | MediaType::Cts
-                    | MediaType::Dts
-                    | MediaType::Dmts
-                    | MediaType::Dcts
-                    | MediaType::Tsx => (ModuleType::JavaScript, true),
-                    MediaType::Json => (ModuleType::Json, false),
-                    _ => {
-                        let extension = path.extension().and_then(|extension| extension.to_str());
+            let (module_type, should_transpile) = match MediaType::from_path(&path) {
+                MediaType::JavaScript | MediaType::Mjs | MediaType::Cjs => {
+                    (ModuleType::JavaScript, false)
+                }
+                MediaType::Jsx => (ModuleType::JavaScript, true),
+                MediaType::TypeScript
+                | MediaType::Mts
+                | MediaType::Cts
+                | MediaType::Dts
+                | MediaType::Dmts
+                | MediaType::Dcts
+                | MediaType::Tsx => (ModuleType::JavaScript, true),
+                MediaType::Json => (ModuleType::Json, false),
+                _ => {
+                    let extension = path.extension().and_then(|extension| extension.to_str());
 
-                        return Err(match extension {
-                            Some(extension) => JsErrorBox::generic(format!(
-                                "Unknown file extension \".{}\" for import \"{}\"",
-                                extension,
-                                path.display(),
-                            ))
-                            .into(),
-                            None => JsErrorBox::generic(format!(
-                                "Missing file extension for import \"{}\"",
-                                path.display()
-                            ))
-                            .into(),
-                        });
-                    }
-                };
+                    return Err(match extension {
+                        Some(extension) => JsErrorBox::generic(format!(
+                            "Unknown file extension \".{}\" for import \"{}\"",
+                            extension,
+                            path.display(),
+                        ))
+                        .into(),
+                        None => JsErrorBox::generic(format!(
+                            "Missing file extension for import \"{}\"",
+                            path.display()
+                        ))
+                        .into(),
+                    });
+                }
+            };
 
-                let code = std::fs::read_to_string(&path).map_err(|_| {
-                    JsErrorBox::generic(format!(
-                        "No such file found for import \"{}\"",
-                        path.display()
-                    ))
-                })?;
+            let code = std::fs::read_to_string(&path).map_err(|_| {
+                JsErrorBox::generic(format!(
+                    "No such file found for import \"{}\"",
+                    path.display()
+                ))
+            })?;
 
-                let code = if should_transpile {
-                    let parsed = deno_ast::parse_module(ParseParams {
-                        specifier: module_specifier.clone(),
-                        text: code.into(),
-                        media_type,
-                        capture_tokens: false,
-                        scope_analysis: false,
-                        maybe_syntax: None,
-                    })
-                    .map_err(JsErrorBox::from_err)?;
+            let code = if should_transpile {
+                let parsed = deno_ast::parse_module(ParseParams {
+                    specifier: module_specifier.clone(),
+                    text: code.into(),
+                    media_type,
+                    capture_tokens: false,
+                    scope_analysis: false,
+                    maybe_syntax: None,
+                })
+                .map_err(JsErrorBox::from_err)?;
 
-                    let source = parsed
-                        .transpile(
-                            &Default::default(),
-                            &Default::default(),
-                            &EmitOptions::default(),
-                        )
-                        .map_err(JsErrorBox::from_err)?
-                        .into_source();
+                let source = parsed
+                    .transpile(
+                        &Default::default(),
+                        &Default::default(),
+                        &EmitOptions::default(),
+                    )
+                    .map_err(JsErrorBox::from_err)?
+                    .into_source();
 
-                    source.text
-                } else {
-                    code
-                };
+                source.text
+            } else {
+                code
+            };
 
-                let module = ModuleSource::new(
-                    module_type,
-                    ModuleSourceCode::String(code.into()),
-                    &module_specifier,
-                    None,
-                );
+            let module = ModuleSource::new(
+                module_type,
+                ModuleSourceCode::String(code.into()),
+                &module_specifier,
+                None,
+            );
 
-                Ok(module)
-            }
-            .boxed_local(),
-        )
+            Ok(module)
+        };
+
+        ModuleLoadResponse::Sync(module_load())
     }
 }
 
