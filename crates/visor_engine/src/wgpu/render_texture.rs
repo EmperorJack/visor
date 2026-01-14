0b74b26d3f4d642e82fab20b283a9bf4f6818410
Jack Purvis <jack.v.purvis@gmail.com>
2026-01-14 22:49:56 +1300

    Resizable render textures

--- crates/visor_engine/src/wgpu/render_texture.rs
+++ crates/visor_engine/src/wgpu/render_texture.rs
@@ -22,6 +22,23 @@ impl RenderTexture {
         height: u32,
         sample_count: u32,
     ) -> Self {
+        let (texture, renderer) =
+            Self::create_graphics(&wgpu_handle.device, width, height, sample_count);
+
+        Self {
+            id,
+            texture,
+            renderer,
+            wgpu_handle,
+        }
+    }
+
+    fn create_graphics(
+        device: &nannou::wgpu::Device,
+        width: u32,
+        height: u32,
+        sample_count: u32,
+    ) -> (nannou::wgpu::Texture, nannou::draw::Renderer) {
         let format = nannou::wgpu::TextureFormat::Rgba16Float;
 
         let texture = nannou::wgpu::TextureBuilder::new()
@@ -32,17 +49,12 @@ impl RenderTexture {
             )
             .sample_count(sample_count)
             .format(format)
-            .build(&wgpu_handle.device);
+            .build(device);
 
         let renderer = nannou::draw::RendererBuilder::new()
-            .build_from_texture_descriptor(&wgpu_handle.device, texture.descriptor());
+            .build_from_texture_descriptor(device, texture.descriptor());
 
-        Self {
-            id,
-            texture,
-            renderer,
-            wgpu_handle,
-        }
+        (texture, renderer)
     }
 
     pub fn id(&self) -> &RenderTextureId {
@@ -55,6 +67,15 @@ impl RenderTexture {
 
     pub fn texture_view(&self) -> nannou::wgpu::TextureView {
         self.texture.view().build()
+    }
+
+    pub fn resize(&mut self, width: u32, height: u32) {
+        (self.texture, self.renderer) = Self::create_graphics(
+            &self.wgpu_handle.device,
+            width,
+            height,
+            self.texture().sample_count(),
+        );
     }
 
     pub fn render(&mut self, draw: &nannou::Draw, encoder: &mut nannou::wgpu::CommandEncoder) {
